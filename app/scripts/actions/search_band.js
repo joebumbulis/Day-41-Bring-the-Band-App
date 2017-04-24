7eff08793e8d82f9f036670abd462eaaec8dfd9d
Joe Bumbulis <joebumbulis@gmail.com>
2017-04-24 15:58:50 -0500

    completed ajax successfully from api

--- app/scripts/actions/search_band.js
+++ app/scripts/actions/search_band.js
@@ -6,7 +6,8 @@ export default function searchBand(searchQuery) {
       type: "GET",
       url: "https://api.spotify.com/v1/search?q=tania%20bowra&type=artist",
       dataType: "json"
-    }).then(response => {
+    }).then(function(response, success) {
+      console.log(success);
       dispatch({ type: "DISPLAY_BANDS", data: response });
     });
   };
