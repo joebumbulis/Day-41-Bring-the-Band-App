96e0ce8d8aa2a6857b9d0c7bb0ce1f9dda557716
Joe Bumbulis <joebumbulis@gmail.com>
2017-04-24 15:49:22 -0500

    updating reducer with data

--- app/scripts/reducers/app_reducer.js
+++ app/scripts/reducers/app_reducer.js
@@ -1,12 +1,21 @@
 export default function AppReducer(state, action) {
   if (state === undefined) {
-    return {};
+    return {
+      searching: false,
+      bands: [],
+      votes: []
+    };
   }
 
   switch (action.type) {
-    case "TEST_REDUCER":
-      // DELETE OR CHANGE ME: I AM JUST AN EXAMPLE
-      return Object.assign({}, state, { newData: action.data });
+    case "SEARCHING_BANDS":
+      return Object.assign({}, state, { searching: true });
+
+    case "DISPLAY_BANDS":
+      return Object.assign({}, state, {
+        searching: false,
+        bands: action.data.artists.item
+      });
   }
 
   console.log("Unhandled State!");
